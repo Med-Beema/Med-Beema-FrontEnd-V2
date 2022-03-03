c9763a22fd1dc4eef846126b268fd646feea41c0
garima <garimasubedi321@gmail.com>
2022-03-03 22:32:48 +0545

    home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -11,7 +11,11 @@ export default function home() {
       <Layout>
         <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu
+            className="ta-navBar"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+          >
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
@@ -19,13 +23,8 @@ export default function home() {
         </Header>
         <Content
           className="site-layout"
-          style={{ padding: "0 50px", marginTop: 64 }}
+          style={{ padding: "0", marginTop: 64 }}
         >
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
           <div
             className="site-layout-background"
             style={{ padding: 24, minHeight: 380 }}
